0af607672ae6c89197671f7c76bb227411224665
Jun Hue <junhue.lim@qanvast.com>
2018-07-31 17:32:23 +0800

    Updated AdminController create admin

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -3,13 +3,13 @@ import { AdminModel } from '../models/AdminModel';
 
 export const createAdmin = async (req, res) => {
     const newAdmin = new AdminModel(req.body);
-    newAdmin.password = newAdmin.generateHash(req.body.password);
+    newAdmin.password = await newAdmin.generateHash(req.body.password);
 
     return newAdmin
         .save()
         .then((em) => {
-            res.json(em);
             logger.info(`Create Admin: ${em._id}`);
+            res.end();
         })
         .catch(err => logger.error(`Error in create Admin: ${err}`));
 };
